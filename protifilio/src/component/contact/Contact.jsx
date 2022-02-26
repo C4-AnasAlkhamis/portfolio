f462629a0cbb5afc7fa5d99fba0b2b6f4e15e8d0
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-02-26 13:34:50 +0300

    contact  component sending email

--- protifilio/src/component/contact/Contact.jsx
+++ protifilio/src/component/contact/Contact.jsx
@@ -1,69 +1,114 @@
 import "./contact.css";
 
-// import emailjs from "emailjs-com";
-
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 const Contact = () => {
-  //   const sendEmail = (e) => {
-  //     sendFeedBack();
-  //     emailjs
-  //       .sendForm(
-  //         "service_0c3knvt",
-  //         "template_02ihw4s",
-  //         form.current,
-  //         "user_HpGxyEqePdddiz4DOkw1R"
-  //       )
-  //       .then(
-  //         (result) => {
-  //         },
-  //         (error) => {}
-  //       );
-  //   };
-
+  const form = useRef();
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(!loading);
+    emailjs
+      .sendForm(
+        "service_hh9kjvr",
+        "template_1v4eifn",
+        form.current,
+        "dRH2DGYZs7prv3I0H"
+      )
+      .then(
+        (result) => {
+          setEmail("");
+          setSubject("");
+          setName("");
+          setMessage("");
+          setLoading(false);
+        },
+        (error) => {}
+      );
+  };
   return (
     <>
-        <form
-          className="feedbackForm"
-          // ref={form} onSubmit={sendEmail}
-        >
+      <div className="center">
+        <form className="emailForm" ref={form} onSubmit={sendEmail}>
           <input
-            id="sendBtn3"
-            type="email"
-            placeholder="Your Email Address"
-            name="email"
-            // onChange={(e) => {
-            //   setEmail(e.target.value);
-            // }}
-          />
-          <input
-            id="sendBtn2"
-            type="text"
-            placeholder="Your Name"
-            name="name"
-            // onChange={(e) => {
-            //   setName(e.target.value);
-            // }}
-          />
-          <input
-            id="sendBtn1"
             type="text"
             placeholder="Subject"
             name="subject"
-            // onChange={(e) => {
-            //   setSubject(e.target.value);
-            // }}
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
           />
+          <input
+            type="email"
+            placeholder="Your Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Your Name"
+            name="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+
           <textarea
-            id="sendBtn4"
             placeholder="Your Message Content"
-            cols="80"
-            rows="10"
             name="message"
-            // onChange={(e) => {
-            //   setFeedback(e.target.value);
-            // }}
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
           />
-          <input id="sendBtn5" type="submit" value="Send FeedBack " />
+
+          <input
+            style={{ opacity: `${loading ? 0 : 1}` }}
+            id="sendBtn5"
+            type="submit"
+            value="Send email "
+          />
         </form>
+        {loading ? (
+          <div className="loadingio-spinner-blocks-flmttbazwc5">
+            <div className="ldio-9c1aqzrcjvl">
+              <div
+                style={{ left: "38px", top: "38px", animationDelay: "0s" }}
+              ></div>
+              <div
+                style={{ left: "80px", top: "38px", animationDelay: "0.125s" }}
+              ></div>
+              <div
+                style={{ left: "122px", top: "38px", animationDelay: "0.25s" }}
+              ></div>
+              <div
+                style={{ left: "38px", top: "80px", animationDelay: "0.875s" }}
+              ></div>
+              <div
+                style={{ left: "122px", top: "80px", animationDelay: "0.375s" }}
+              ></div>
+              <div
+                style={{ left: "38px", top: "122px", animationDelay: "0.75s" }}
+              ></div>
+              <div
+                style={{ left: "80px", top: "122px", animationDelay: "0.625s" }}
+              ></div>
+              <div
+                style={{ left: "122px", top: "122px", animationDelay: "0.5s" }}
+              ></div>
+            </div>
+          </div>
+        ) : null}
+      </div>
     </>
   );
 };
