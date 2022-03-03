b9cac0523a7e04ce61cb0d372642458788fc3f69
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-03-03 15:02:27 +0300

    tree component makeing DS tree

--- src/component/trees/Trees.jsx
+++ src/component/trees/Trees.jsx
@@ -1,158 +1,161 @@
 import "./trees.css";
 import { useEffect, useState } from "react";
-class Node {
-  constructor(data = null) {
-    this.node = { data: data, children: [] };
-  }
-  add(element) {
+// class Node {
+//   constructor(data = null) {
+//     this.node = { data: data, children: [] };
+//   }
+
+//   remove(element) {
+//     if (!element || !this.node) {
+//       return "No Value to delete";
+//     }
+//     let newNode = this.node;
+//     if (this.node.data === element) {
+//       this.node = { data: null, children: [] };
+//       return;
+//     }
+//     while (newNode) {
+//       if (!newNode.children.length) {
+//         return "Not match";
+//       }
+//       if (element > newNode.data) {
+//         if (newNode.children[1].data === element) {
+//           newNode.children[1] = null;
+//           return;
+//         } else {
+//           newNode = newNode.children[1];
+//         }
+//       } else if (element < newNode.data) {
+//         if (newNode.children[0].data === element) {
+//           newNode.children[0] = null;
+//           return;
+//         } else {
+//           newNode = newNode.children[0];
+//         }
+//       }
+//     }
+//   }
+// }
+
+// class Tree {
+//   constructor(root = null) {
+//     this.root = root;
+//   }
+//   // this method should iterate over the tree, then it should invoke a function in every data inside the tree by level from left to right
+//   traverseBF(fn) {
+//     let index = 0;
+//     let arr = [this.root.node];
+//     while (arr.length) {
+//       let node;
+//       if (arr[0]) {
+//         node = arr.shift();
+//       } else {
+//         arr = arr.slice(1);
+//         node = arr.shift();
+//       }
+//       if (node.children[0] || node.children[1]) {
+//         arr.push(...node.children);
+//       }
+//       index++;
+//       fn(node.data);
+//     }
+//   }
+// }
+function Trees() {
+  const nodes = [];
+  const [nodeList, setNodeList] = useState([]);
+  const [treeNode, setTreeNode] = useState("");
+  const [data, setData] = useState("");
+  const [tree, setTree] = useState([]);
+  let nodeCopy = tree;
+  // Add function
+  // --------------------------------
+  const add = (element) => {
+    let newNode = nodeCopy;
     if (!element) {
       return "No Value to add";
-    }
-    let newNode = this.node;
-    if (!this.node || this.node.data === null) {
-      this.node = { data: element, children: [] };
-    }
-    if (element === newNode.data) {
-      return "already exist";
-    }
-    while (newNode) {
-      if (element === newNode.data) {
-        return "already exist";
-      }
-      if (element > newNode.data) {
-        if (!newNode.children[1]) {
-          newNode.children[1] = { data: element, children: [] };
-          return;
-        } else {
-          newNode = newNode.children[1];
-        }
-      } else if (element < newNode.data) {
-        if (!newNode.children[0]) {
-          newNode.children[0] = { data: element, children: [] };
-          return;
-        } else {
-          newNode = newNode.children[0];
-        }
-      }
-    }
-  }
-  remove(element) {
-    if (!element || !this.node) {
-      return "No Value to delete";
-    }
-    let newNode = this.node;
-    if (this.node.data === element) {
-      this.node = { data: null, children: [] };
+    } else if (nodeCopy.length === 0) {
+      nodeCopy[0] = { data: element, children: [] };
+      setTree(nodeCopy[0]);
+      setNodeList([nodeCopy[0].data]);
+      setData("");
       return;
-    }
-    while (newNode) {
-      if (!newNode.children.length) {
-        return "Not match";
-      }
-      if (element > newNode.data) {
-        if (newNode.children[1].data === element) {
-          newNode.children[1] = null;
-          return;
-        } else {
-          newNode = newNode.children[1];
+    } else {
+      while (newNode) {
+        if (parseInt(element) === parseInt(newNode.data)) {
+          return "already exist";
         }
-      } else if (element < newNode.data) {
-        if (newNode.children[0].data === element) {
-          newNode.children[0] = null;
-          return;
-        } else {
-          newNode = newNode.children[0];
+        if (parseInt(element) !== parseInt(newNode.data)) {
+          setNodeList([...nodeList, element]);
         }
-      }
-    }
-  }
-}
 
-class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
-  // this method should iterate over the tree, then it should invoke a function in every data inside the tree by level from left to right
-  traverseBF(fn) {
-    let index = 0;
-    let arr = [this.root.node];
+        setData("");
+        if (parseInt(element) > parseInt(newNode.data)) {
+          if (!newNode.children[1]) {
+            newNode.children[1] = { data: element, children: [] };
+            setTree(nodeCopy);
+            return;
+          } else {
+            newNode = newNode.children[1];
+          }
+        } else if (parseInt(element) < parseInt(newNode.data)) {
+          if (!newNode.children[0]) {
+            newNode.children[0] = { data: element, children: [] };
+            setTree(nodeCopy);
+            return;
+          } else {
+            newNode = newNode.children[0];
+          }
+        }
+      }
+    }
+  };
+
+  //   // this method should iterate over the tree, then it should invoke a function in every data inside the tree by level from left to right
+  const traverseBF1 = () => {
+    let arr = [nodeCopy];
+    let node;
     while (arr.length) {
-      let node;
       if (arr[0]) {
         node = arr.shift();
       } else {
         arr = arr.slice(1);
         node = arr.shift();
       }
-      if (node.children[0] || node.children[1]) {
+      if ((node && node.children[0]) || (node && node.children[1])) {
         arr.push(...node.children);
       }
-      index++;
-      fn(node.data);
+      nodes.push(node.data);
+      setTreeNode(nodes);
     }
-  }
-  // this method should iterate over the tree, then it should invoke a function in every data inside the tree where(root~left~right)in order
-  traverseDF(fn) {
-    let arr = [this.root.node];
-    while (arr.length) {
-      let node;
-      if (arr[arr.length - 1]) {
-        node = arr.shift();
-      } else {
-        arr = arr.slice(0, arr.length - 2);
-        node = arr.shift();
-      }
-      if ((!node && node.children[0]) || node.children[1]) {
-        arr.unshift(...node.children);
-      }
-
-      fn(node.data);
-    }
-  }
-}
-function Trees() {
-  const [data, setData] = useState(null);
-  const nodes = [];
-
-  const node = new Node(25);
-  const tree = new Tree(node);
-  node.add(10);
-  node.add(7);
-  node.add(9);
-  node.add(3);
-  node.add(2);
-  node.add(14);
-  node.add(12);
-  node.add(15);
-  node.add(29);
-  node.add(4);
-  node.add(30);
-  node.add(35);
-
-  tree.traverseBF((node, i) => {
-    nodes.push(node);
-    console.log(node);
-  });
-  const addNode = () => {
-    node.add(parseInt(data));
-    console.log(node);
-    tree.traverseBF((node, i) => {
-      nodes.push(node);
-      console.log(node);
-    });
   };
+
   return (
     <div className="Tree_container">
       <div className="tree_box">
-        {nodes.map((node, i) => {
-          return (
-            <div className="node" key={i} id={i}>
-              <span>{node}</span>
-            </div>
-          );
-        })}
+        {treeNode
+          ? treeNode.map((node, i) => {
+              console.log(node);
+            
+              // return (
+              //   <div style={{ top: 0 }} className="node" key={i} id={i}>
+              //     <span>{node}</span>
+              //   </div>
+              // );
+            })
+          : null}
       </div>
-
+      <div className="nodes_list">
+        {nodeList
+          ? nodeList.map((node, i) => {
+              return (
+                <div className="list" key={i} id={i}>
+                  <span>{node}</span>
+                </div>
+              );
+            })
+          : null}
+      </div>
       <div>
         <input
           onChange={(e) => {
@@ -165,14 +168,18 @@ function Trees() {
 
         <button
           onClick={() => {
-            addNode();
-            // node.add(parseInt(data));
-            // console.log(node);
+            add(data);
           }}
         >
           add
         </button>
-        <button>remove</button>
+        <button
+          onClick={() => {
+            traverseBF1();
+          }}
+        >
+          Render
+        </button>
       </div>
     </div>
   );
