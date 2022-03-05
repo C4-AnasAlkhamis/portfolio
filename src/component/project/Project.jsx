3b840e26682a2da8dfbbb8abdd952ba5404831f0
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-03-05 21:37:42 +0300

    project slider 5

--- src/component/project/Project.jsx
+++ src/component/project/Project.jsx
@@ -41,23 +41,71 @@ const project_tow = [
 const Project = () => {
   return (
     <div className="project_container">
-      <div className="project_one">
-        {project_one.map((img, i) => {
-          return (
-            <span style={{ "--i": i + 1 }}>
-              <img key={i} src={img.src} alt={img.alt} />
-            </span>
-          );
-        })}
+      <div className="project_one_box">
+        <div className="project_one_info_box">
+          <h2>E-commerce & Services</h2>
+          <div className="skills_interests">
+            <ul>
+              <li>Project name: TechniCorner</li>
+              <li>
+                An easy-to-use interactive website, that provides a variety of
+                products that serve the industry sector.
+              </li>
+              <li>
+                An easy-to-use interactive website, that provides a variety of
+                products that serve the industry sector.
+              </li>
+              <li className="technology_box">
+                Technology used
+                <span>Backend: Node.js, Express, Socket.io</span>
+                <span>Frontend: React.js, CSS3</span>
+                <span>Database: MySQL</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="project_one">
+          {project_one.map((img, i) => {
+            return (
+              <span style={{ "--i": i }}>
+                <img key={i} src={img.src} alt={img.alt} />
+              </span>
+            );
+          })}
+        </div>
       </div>
-      <div className="project_tow">
-        {project_tow.map((img, i) => {
-          return (
-            <span style={{ "--i": i + 1 }}>
-              <img key={i} src={img.src} alt={img.alt} />
-            </span>
-          );
-        })}
+      <div className="project_tow_box">
+        <div>
+          <h2>social media for players</h2>
+          <div className="skills_interests">
+            <ul>
+              <li>social media for players</li>
+              <li>
+                Focuses on video games players, and offers them different
+                advantages
+              </li>
+              <li>
+                different advantages The website is intended for social
+                networking and exchanging information and experiences..
+              </li>
+              <li className="technology_box">
+                Technology used
+                <span>Backend: Node.js, Express, Socket.io</span>
+                <span>Frontend: React.js, CSS3</span>
+                <span>Database: MongoDB</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="project_tow">
+          {project_tow.map((img, i) => {
+            return (
+              <span style={{ "--i": i }}>
+                <img key={i} src={img.src} alt={img.alt} />
+              </span>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
